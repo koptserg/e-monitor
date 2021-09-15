81fe4c8627b02483408efab42e1b04b8599f8b9d
Sergey Koptyakov <52034595+koptserg@users.noreply.github.com>
2021-09-15 22:29:47 +0500

    Add files via upload

---
+++ Source/version.c
@@ -0,0 +1,18 @@
+
+    #ifndef ZCL_APP_VERSION_H
+    #define ZCL_APP_VERSION_H
+
+    #ifdef __cplusplus
+    extern "C" {
+    #endif
+
+    #include "version.h"
+const uint8 zclApp_DateCode[] = { 16, '1', '5', '/', '0', '9', '/', '2', '0', '2', '1', ' ', '2', '2', ':', '1', '2' };
+const char zclApp_DateCodeNT[] = "15/09/2021 22:12";
+
+    #ifdef __cplusplus
+    }
+    #endif
+
+    #endif /* ZCL_APP_VERSION_H */
+    
